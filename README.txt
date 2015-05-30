385d87c5325794e79e461bce0b23d2da90d071d8
Aleksandra <spb.1118332@gmail.com>
2015-05-30 16:17:08 +0300

    Done

---
+++ README.txt
@@ -0,0 +1,6 @@
+> Как установить мой проект
+
+1. git clone
+2. npm i
+3. bower i
+4. gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 
 var gulp = require("gulp"),
+wiredep = require('wiredep').stream,
+useref = require('gulp-useref'),
+gulpif = require('gulp-if'),
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-minify-css'),
 browserSync = require('browser-sync');
 
+// запуск локального сервера
 gulp.task('server', function () { 
 	browserSync({
 	port: 9000,
@@ -11,6 +17,7 @@ gulp.task('server', function () {
 	}); 
 });
 
+// слежка за изменениями
 gulp.task('watch', function () {
 gulp.watch([
 'app/*.html',
@@ -20,3 +27,23 @@ gulp.watch([
 });
 
 gulp.task('default', ['server', 'watch']);
+
+// переписывает bower зависимость
+gulp.task('wiredep', function () { 
+gulp.src('app/*.html')
+.pipe(wiredep())
+.pipe(gulp.dest('app'));
+});
+
+// 
+gulp.task('dist', function () {
+var assets = useref.assets();
+
+return gulp.src('app/*.html')
+.pipe(assets)
+.pipe(gulpif('*.js', uglify()))
+.pipe(gulpif('*.css', minifyCss()))
+.pipe(assets.restore())
+.pipe(useref())
+.pipe(gulp.dest('dist'));
+});
